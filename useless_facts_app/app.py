d81dc7c27e73bc8a1060ea51251ece4894cb7ae5
ivyanneh <167083813+ivyanneh@users.noreply.github.com>
2025-01-25 21:13:35 +0300

    changes

--- useless_facts_app/app.py
+++ useless_facts_app/app.py
@@ -1,26 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import requests
 
+app = Flask(__name__)
 
-app = Flask(__name__)#instance
-
-BASE_URL = "https://uselessfacts.jsph.pl/api/v2/facts"
-
-
-@app.route('/')#for homepage
-def index():
-   
-    url = f"{BASE_URL}/random?language=en"
-    response = requests.get(url, headers={"Accept": "application/json"})#url for fetching a random fact
-
-         
-   
+# Function to get a useless fact
+def get_useless_fact():
+    base_url ="https://uselessfacts.jsph.pl/"
+    url = "api/v2/facts/random?language=en"
+    response = requests.get(url)
     if response.status_code == 200:
-        fact = response.json()["text"]
-    else:
-        fact = "Sorry, we couldn't fetch a fact at the moment."
+        return response.json()["text"]
+    return response.json()["Could not fetch fact."]
 
-    return render_template('index.html', fact=fact)
+# Function to get a random meme
+def get_meme():
+    url = "https://meme-api.com/gimme"
+    response = requests.get(url)
+    if response.status_code == 200:
+        meme_data = response.json()
+        return {"title": meme_data["title"], "url": meme_data["url"], "postLink": meme_data["postLink"]}
+    return {"title": "Could not fetch meme.", "url": "", "postLink": ""}
 
-if __name__ == '__main__':#running the flask
+@app.route("/")
+def home():
+    fact = get_useless_fact()
+    meme = get_meme()
+    return render_template("index.html", fact=fact, meme=meme)
+
+# API to fetch only a new fact
+@app.route("/get-fact")
+def fetch_fact():
+    return jsonify({"fact": get_useless_fact()})
+
+# API to fetch only a new meme
+@app.route("/get-meme")
+def fetch_meme():
+    return jsonify(get_meme())
+
+if __name__ == "__main__":
     app.run(debug=True)
